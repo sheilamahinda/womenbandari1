8e9e8cfe95627c4e98a219ef73b92399637b0e6d
Sheila Mahinda <113583295+sheilamahinda@users.noreply.github.com>
2025-05-02 10:24:50 +0300

    Update useGetMessages.jsx

--- client/src/hooks/useGetMessages.jsx
+++ client/src/hooks/useGetMessages.jsx
@@ -8,17 +8,15 @@ import { ElearningContext } from "../context/ElearningContext";
 const useGetMessages = () => {
   const { _id } = useContext(ElearningContext);
   const { selectedUser } = useSelector((store) => store.user);
-
   const dispatch = useDispatch();
   //          `http://localhost:3005/api/v1/message/${selectedUser?._id}`
-
   useEffect(() => {
     const fetchMessages = async () => {
       try {
         axios.defaults.withCredentials = true;
         
         const res = await axios.get(
-          `${process.env.REACT_APP_BACKEND_URI/api/v1/message/${selectedUser?._id}`
+          `${process.env.REACT_APP_BACKEND_URI}/api/v1/message/${selectedUser?._id}`
         );
         dispatch(setMessages(res.data));
       } catch (error) {
